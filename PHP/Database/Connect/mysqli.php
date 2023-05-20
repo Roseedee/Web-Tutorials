14d357976fe680fa1afc7549dbcb9d857221e7d9
Roseedee Cehleah <roseedee2002@gmail.com>
2023-05-20 14:06:29 +0700

    Create mysqli.php
    connect database by mysqli

---
+++ PHP/Database/Connect/mysqli.php
@@ -0,0 +1,15 @@
+<?php
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $db_name = "test";
+
+    $conn = new mysqli($host, $username, $password, $db_name);
+
+    if($conn->connect_error) {
+        echo "Connection failed: " . $conn->connect_error;
+    }
+
+    echo "Connect Successfully";
+
+?>

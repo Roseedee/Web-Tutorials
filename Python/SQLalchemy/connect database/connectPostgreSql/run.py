aa8675acfa6c03cae1a135131d09aaffe1e1b284
Roseedee Cehleah <roseedee2002@gmail.com>
2023-07-11 15:32:51 +0700

    Update run.py

--- Python/SQLalchemy/connect database/connectPostgreSql/run.py
+++ Python/SQLalchemy/connect database/connectPostgreSql/run.py
@@ -1,3 +1,19 @@
 from sqlalchemy import create_engine
 
-engine = create_engine('postgresql+psycopg2://user:password\@hostname/database_name')
+db_host = "localhost"
+db_username = "postgres"
+db_password = "12345"
+db_name = "postgres"
+
+engine = create_engine(f"postgresql://{db_username}:{db_password}@{db_host}:{5432}/{db_name}")
+
+con = engine.connect()
+
+if con.closed:
+    print("connect database failed")
+else:
+    print("connect database successfully")
+
+con.close()
+
+

27deb437f580d408d48698269e3cf0ffbeeba332
Roseedee Cehleah <roseedee2002@gmail.com>
2023-07-12 11:40:04 +0700

    Update run.py

--- Python/SQLalchemy/model table/run.py
+++ Python/SQLalchemy/model table/run.py
@@ -1,12 +1,29 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text, MetaData, Table, Column, Integer, String, insert, Null, select
 
-engine = create_engine(f"postgresql://postgres:12345@localhost/postgres")
 
-con = engine.connect()
+user_tb = Table('user_tb', MetaData(),  Column('id', Integer, primary_key=True), Column("f_name", String, nullable=False),Column("l_name", String, nullable=False), Column("age", Integer, nullable=False))
 
-if con.connection:
-    r = con.execute('select * from "user" order by "id"')
+
+db_connection = "postgresql://postgres:12345@localhost/postgres"
+
+engine = create_engine(db_connection)
+
+with engine.connect() as con:
+    stmt = select(user_tb)
+    r = con.execute(stmt)
     for i in r:
         print(i)
+    # con.execute(
+    #     text('insert into "user_tb" ("f_name", "l_name", "age") values (:fname, :lname, :age)'),
+    #     [
+    #         {
+    #             "fname" : "Roseedee",
+    #             "lname" : "Cehlaeh",
+    #             "age" : 21
+    #         }
+    #     ]
+    # )
+    
+    
 
     
